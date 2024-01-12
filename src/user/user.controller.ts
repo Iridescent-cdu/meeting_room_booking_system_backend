c2e06d0961a87f28f8493a021f2530cb326504ae
iridescent <2389504513@qq.com>
2024-01-12 22:36:28 +0800

    ✨ feat: 新增注册接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/registerUser.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  register(@Body() registerUser: RegisterUserDto) {}
 }
