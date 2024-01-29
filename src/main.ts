67df82d603eae13f1dc64e66fb2325c960592327
iridescent <2389504513@qq.com>
2024-01-29 11:47:59 +0800

    ✨ feat: 新增未登录异常过滤器

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { FormatResponseInterceptor } from './format-response.interceptor';
 import { InvokeRecordInterceptor } from './invoke-record.interceptor';
+import { UnLoginFilter } from './unlogin.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new FormatResponseInterceptor());
   app.useGlobalInterceptors(new InvokeRecordInterceptor());
+  app.useGlobalFilters(new UnLoginFilter());
 
   const configService = app.get(ConfigService);
   const port = configService.get('nest_server_port');
