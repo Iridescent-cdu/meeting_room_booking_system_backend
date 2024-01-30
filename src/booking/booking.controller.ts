6a782f78f8af2db037bcabe266490531e4680e8d
iridescent <2389504513@qq.com>
2024-01-30 23:00:42 +0800

    ✨ feat: 新增预定信息分页查询接口

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, DefaultValuePipe, Get, Query } from '@nestjs/common';
 import { BookingService } from './booking.service';
+import { generateParseIntPipe } from 'src/utils';
 
 @Controller('booking')
 export class BookingController {
   constructor(private readonly bookingService: BookingService) {}
+  @Get('list')
+  async list(
+    @Query('pageNo', new DefaultValuePipe(1), generateParseIntPipe('pageNo'))
+    pageNo: number,
+    @Query(
+      'pageSize',
+      new DefaultValuePipe(10),
+      generateParseIntPipe('pageSize'),
+    )
+    pageSize: number,
+    @Query('username') username: string,
+    @Query('meetingRoomName') meetingRoomName: string,
+    @Query('meetingRoomPosition') meetingRoomPosition: string,
+    @Query('bookingTimeRangeStart') bookingTimeRangeStart: number,
+    @Query('bookingTimeRangeEnd') bookingTimeRangeEnd: number,
+  ) {
+    return this.bookingService.find(
+      pageNo,
+      pageSize,
+      username,
+      meetingRoomName,
+      meetingRoomPosition,
+      bookingTimeRangeStart,
+      bookingTimeRangeEnd,
+    );
+  }
 }
--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { MeetingRoom } from 'src/meeting-room/entities/meeting-room.entity';
 import { User } from 'src/user/entities/user.entity';
-import { EntityManager } from 'typeorm';
+import { Between, EntityManager, Like } from 'typeorm';
 import { Booking } from './entities/booking.entity';
 import { InjectEntityManager } from '@nestjs/typeorm';
 
@@ -56,5 +56,77 @@ export class BookingService {
     booking4.endTime = new Date(Date.now() + 1000 * 60 * 60);
 
     await this.entityManager.save(Booking, booking4);
+  }
+
+  async find(
+    pageNo: number,
+    pageSize: number,
+    username: string,
+    meetingRoomName: string,
+    meetingRoomPosition: string,
+    bookingTimeRangeStart: number,
+    bookingTimeRangeEnd: number,
+  ) {
+    const skipCount = (pageNo - 1) * pageSize;
+
+    const condition: Record<string, any> = {};
+
+    if (username) {
+      condition.user = {
+        username: Like(`%${username}%`),
+      };
+    }
+
+    if (meetingRoomName) {
+      condition.room = {
+        name: Like(`%${meetingRoomName}%`),
+      };
+    }
+
+    if (meetingRoomPosition) {
+      if (!condition.room) {
+        condition.room = {};
+      }
+      condition.room.location = Like(`%${meetingRoomPosition}%`);
+    }
+
+    if (bookingTimeRangeStart) {
+      if (!bookingTimeRangeEnd) {
+        bookingTimeRangeEnd = bookingTimeRangeStart + 60 * 60 * 1000;
+      }
+      condition.startTime = Between(
+        new Date(bookingTimeRangeStart),
+        new Date(bookingTimeRangeEnd),
+      );
+    }
+
+    const [bookings, totalCount] = await this.entityManager.findAndCount(
+      Booking,
+      {
+        // select: {
+        //   id: true,
+        //   startTime: true,
+        //   user: {
+        //     id: true,
+        //     nickName: true,
+        //   },
+        // },
+        where: condition,
+        relations: {
+          user: true,
+          room: true,
+        },
+        skip: skipCount,
+        take: pageSize,
+      },
+    );
+
+    return {
+      bookings: bookings.map((item) => {
+        delete item.user.password;
+        return item;
+      }),
+      totalCount,
+    };
   }
 }
